3da2b0f0be581af1999c54284103195de6507073
Gulilil <juan.csantoso@gmail.com>
2022-09-15 02:32:17 +0700

    building main program

--- README.md
+++ README.md
@@ -1,1 +1,49 @@
-# Tubes1_Algeo
+<h1> <center> Tugas Besar 1 <br> 
+Aljabar Linier dan Geometri </center> </h1>
+
+<h2> <b> Kelompok 7 - ImagineKelar </b> </h2>
+<h3> Anggota Kelompok: 
+<ol> 
+<li> Juan Christopher Santoso - 13521116 
+<li> Nicholas Liem  - 13521135 
+<li> Nathania Calista Djunaedi - 1352139
+</ol> </h3>
+
+<h2> <b> Pembagian Tugas </b> </h2>
+<p> List tugas pada Source Code : </p>
+<ul>
+<li> Sistem Persamaan Linear ( Gauss & Gauss-Jordan )
+<li> Sistem Persamaan Linear ( Matriks Balikan & Kramer )
+<li> Determinan
+<li> Matriks Balikan
+<li> Interpolasi Polinom 
+<li> Interpolasi Bicubic + Bonus
+<li> Regresi Linear Berganda
+<li> Fungsi dan Prosedur lainnya:
+    <ul>
+    <li> Create matriks
+    <li> Read matriks
+    <li> Print matriks
+    <li> Swap (tukar baris)
+    <li> Transpose 
+    <li> ( ya ini nanti ditambahin aja sendiri lah )
+    </ul>
+</ul>
+
+<p> List tugas pada Laporan </p>
+<ul>
+<li> Cover 
+<li> Daftar Isi
+<li> Daftar Gambar dan Tabel ( Jika ada) 
+<li> Bab 1 - Deskripsi masalah
+<li> Bab 2 - Teori Singkat 
+<li> Bab 3 - Implementasi Pustaka dan Program Java
+<li> Bab 4 - Eksperimen (Hasil Eksekusi Program)
+<li> Bab 5 - Kesimpulan, saran, dan refleksi
+<li> Daftar Pustaka
+<li> Lampiran
+
+</ul>
+
+<h2> <b> Keterangan Tambahan Lain </b> </h2>
+<p> ~ belum ada ~ </p>
---
+++ source code/Main.java
@@ -0,0 +1,123 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        // program adalah variabel yang menunjukkan bahwa program masih berjalan 
+        // program bernilai true apabila Program masih dijalankan. Di sisi lain, bernilai false bila Program telah selesai dijalankan
+        boolean program = true;
+        boolean subprogram1 = false;
+        Scanner scanObj = new Scanner(System.in);
+        int userNumber;         // untuk input pada main menu
+        int userSubNumber;      // untuk input pada sub main menu
+        while (program) {
+            System.out.println("=================================");
+            System.out.println("=====       MAIN MENU       =====");
+            System.out.println("=================================");
+            System.out.println("1. Sistem Persamaan Linier");
+            System.out.println("2. Determinan");
+            System.out.println("3. Matriks Balikan");
+            System.out.println("4. Interpolasi Polinom");
+            System.out.println("5. Interpolasi Bicubic");
+            System.out.println("6. Regresi Linier Berganda");
+            System.out.println("7. Keluar");
+            System.out.print("Masukkan angkanya saja (1-7): ");
+            
+            // Membaca input dari user
+            userNumber = scanObj.nextInt();
+
+            // Meminta ulang input dari user bila input yang diberikan tidak valid. Diberikan pesan apabila input tidak valid
+            if ((userNumber < 1) || (userNumber > 7)){
+                System.out.println("Input "+ userNumber +" tidak valid. Silahkan masukan input yang valid.");
+            }
+            // Bila input yang diberikan bersifat valid
+            else {
+
+                // User memilih fitur pertama
+                if ( userNumber == 1){
+                    // variabel subprogram bernilai true ( dijalankan )
+                    subprogram1 = true;
+                    while (subprogram1) {
+                        System.out.println("=================================");
+                        System.out.println("======       MENU  1       ======");
+                        System.out.println("=================================");
+                        System.out.println("1. Metode Eliminasi Gauss");
+                        System.out.println("2. Metode Eliminasi Gauss-Jordan");
+                        System.out.println("3. Metode Matriks Balikan");
+                        System.out.println("4. Kaidah Cramer");
+                        System.out.println("5. Kembali ke Main Menu");
+
+                        System.out.print("Masukkan angkanya saja (1-5): ");
+
+                        // Membaca input dari user
+                        userSubNumber = scanObj.nextInt();
+
+                        // Jika inputnya tidak valid, maka akan diberikan pesan bahwa input tidak valid
+                        if ((userSubNumber < 1) || (userSubNumber > 5)){
+                            System.out.println("Input "+ userSubNumber +" tidak valid. Silahkan masukan input yang valid.");
+                        } else {
+
+                            // User memilih fitur pertama
+                            // if ( userSubNumber == 1){
+
+                            // }
+
+                            
+                            // User memilih fitur kedua
+                            // if ( userSubNumber == 2){
+
+                            // }
+
+                            // User memilih fitur ketiga
+                            // if ( userSubNumber == 3){
+
+                            // }
+
+                            // User memilih fitur keempat
+                            // if ( userSubNumber == 4){
+
+                            // }
+
+                            // User memilih fitur kelima
+                            if (userSubNumber == 5) {
+                                subprogram1 = false;
+                            }
+                        }
+                    }
+                }
+
+                // User memilih fitur kedua
+                // if ( userNumber == 2){
+
+                // }
+
+                // User memilih fitur ketiga
+                // if ( userNumber == 3){
+
+                // }
+
+                // User memilih fitur keempat
+                // if ( userNumber == 4){
+
+                // }
+
+                // User memilih fitur kelima
+                // if ( userNumber == 5){
+
+                // }
+
+                // User memilih fitur keenam
+                // if ( userNumber == 6){
+
+                // }
+
+
+                // User memilih fitur ketujuh
+                if ( userNumber == 7){
+                    program = false;
+                }
+            }
+        }
+        // Menutup scanner 
+        scanObj.close();
+    }
+}
