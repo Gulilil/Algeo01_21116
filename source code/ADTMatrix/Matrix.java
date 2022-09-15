c77390fb45f0b15cc3a895d43f0b4b256a70df70
energizedNachos <88883885+energizedNachos@users.noreply.github.com>
2022-09-15 20:57:34 +0700

    Update Matrix.java

--- source code/ADTMatrix/Matrix.java
+++ source code/ADTMatrix/Matrix.java
@@ -1,14 +1,29 @@
 package ADTMatrix;
+import java.io.*;
+import java.util.*;
 
 public class Matrix{
-    int rows = 0;
-    int cols = 0;
+    int rowLength = 0;
+    int colLength = 0;
     double[][] matrix;
+
+    Scanner scanObj = new Scanner(System.in);
 
     public Matrix(int rows, int cols){
         matrix = new double[rows][cols];
-        this.rows = rows;
-        this.cols = cols;
+        this.rowLength = rows;
+        this.colLength = cols;
+    }
+
+    public Matrix(double[][] matrix){
+        rowLength = matrix.length;
+        colLength = matrix[0].length;
+        this.matrix = new double[rowLength][colLength];
+        for(int rows = 0; rows < rowLength; rows++){
+            for(int cols = 0; cols < colLength; cols++){
+                this.matrix[rows][cols] = matrix[rows][cols];
+            }
+        }
     }
 
 }
